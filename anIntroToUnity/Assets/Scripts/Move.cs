10c4488cbbad7863833e96304d0c5f7e37f37987
phuong0 <phuongunguyen4@gmail.com>
2020-04-11 15:08:54 -0700

    Added "mc" and Added ground
    Made a character and made some floor where character can walk on. Next step is to make the character move with arrow keys

---
+++ anIntroToUnity/Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Vector3 speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(speed);
+    }
+}
