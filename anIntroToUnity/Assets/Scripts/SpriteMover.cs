895a10cd9d4b67f1f9e991ee3615472236eb35d6
phuong0 <phuongunguyen4@gmail.com>
2020-05-12 13:17:45 -0700

    Player can jump and level is playable.
    added to the spriteMover scrpt and moved around platforms so player is able to move through easier.

--- anIntroToUnity/Assets/Scripts/SpriteMover.cs
+++ anIntroToUnity/Assets/Scripts/SpriteMover.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.GameCenter;
 
 public class SpriteMover : MonoBehaviour
 {
 
     public float speed = 5;
     private Rigidbody2D rb;
+    private BoxCollider2D pCollider;
     private float currentSpeed;
+    public float jumpForce = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        pCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -21,14 +25,24 @@ public class SpriteMover : MonoBehaviour
         currentSpeed = 0.0f;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            currentSpeed += speed;
+            currentSpeed -= speed;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            currentSpeed -= speed;
+            currentSpeed += speed;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
-        //add jumping
         rb.AddForce(new Vector2(currentSpeed * Time.deltaTime, 0.0f), ForceMode2D.Impulse);
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(pCollider.bounds.center, pCollider.bounds.size, 0f, Vector2.down, 1f);
+        return raycastHit;
     }
 }
