8113bced004a08b4a0e4d9786cd49862c6a46f00
phuong0 <phuongunguyen4@gmail.com>
2020-05-30 15:52:06 -0700

    added enemy and their behavior
    added health, enemy, trigger boxes, detection. casting circle

---
+++ anIntroToUnity/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public float chaseSpeed = 2.0f;
+    public float detectionRadius = 2.0f;
+    public float disengageRadius = 4.0f;
+
+    private bool isFacingRight = true;
+    private Vector2 facing = Vector2.right;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private GameObject chasee;
+
+    private float currentSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+
+        currentSpeed = speed;
+        chasee = null;
+    }
+
+    private GameObject AcquireTarget()
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, detectionRadius, Vector2.right, Mathf.Epsilon, LayerMask.GetMask("Player"));
+
+        if (hit.collider != null)
+        {
+            return hit.collider.gameObject;
+        }
+
+        return null;
+    }
+
+    private void ChaseTarget()
+    {
+        //hit returns the position of the thing that hit it
+        Vector3 playerPosition = chasee.transform.position;
+        Vector3 playerDir = playerPosition - transform.position;
+
+        float dot = Vector3.Dot(Vector3.Normalize(playerDir), new Vector3(facing.x, facing.y, 0.0f));
+
+        if (dot < 0.0f)
+        {
+            Flip();
+        }
+        currentSpeed = chaseSpeed;
+
+        Debug.DrawLine(playerPosition, transform.position, Color.red);
+    }
+
+    public void ShouldDisengage()
+    {
+        float distance = Vector3.Distance(chasee.transform.position, transform.position);
+        if (disengageRadius < distance)
+        {
+            chasee = null;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        currentSpeed = speed;
+
+        if (chasee == null)
+        {
+            GameObject target = AcquireTarget();
+
+            if (target != null)
+            {
+                chasee = target;
+                ChaseTarget();
+            }
+        }
+        else
+        {
+            ChaseTarget();
+        }
+
+
+        Vector2 currentPosition = rb.position;
+        Vector2 newPosition = currentPosition + (facing * currentSpeed * Time.deltaTime);
+
+        rb.MovePosition(newPosition);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        sprite.flipX = !sprite.flipX;
+
+        if (isFacingRight)
+        {
+            facing = Vector2.right;
+        }
+        else
+        {
+            facing = Vector2.left;
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D theCollider)
+    {
+        if (theCollider.gameObject.CompareTag("Patrol"))
+        {
+            Flip();
+            chasee = null;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = new Color(1.0f, 0.0f, 0.0f, 0.2f);
+        Handles.DrawSolidDisc(transform.position, Vector3.back, detectionRadius);
+    }
+}
---
+++ anIntroToUnity/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerData : MonoBehaviour
+{
+    public Slider healthBarUI;
+
+    public Transform spawnPoint;
+    public GameObject gameUI;
+    public GameObject gameOver;
+
+    private float currentHealth = 1.0f;
+
+    public void HitObstacle(float damage)
+    {
+        currentHealth -= damage;
+        UpdateHealth();
+
+        if (currentHealth <= 0.0f)
+        {
+            gameUI.SetActive(false);
+            gameOver.SetActive(true);
+        }
+    }
+
+    private void UpdateHealth()
+    {
+        healthBarUI.value = currentHealth;
+    }
+
+    public bool isGameOver()
+    {
+        return (currentHealth <= 0.0f);
+    }
+
+    public void Respawn()
+    {
+        currentHealth = 1.0f;
+
+        UpdateHealth();
+
+        gameObject.transform.position = spawnPoint.position;
+    }
+}
